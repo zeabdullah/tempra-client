e54cea656578cbe5674994a608b9759f8335188c
Abdullah Z <dev@zabd.dev>
2025-07-17 03:20:38 +0300

    feat: initial setup for react router

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />,
+    },
+]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <>
+            <h1 className="fs-h1">Hello, index page</h1>
+            <h2 className="fs-h2">Hello, index page h2</h2>
+        </>
+    );
+}
