dbf6e45e7cf5ee2742cd2070d8e78e41d4251a4e
Abdullah Z <dev@zabd.dev>
2025-07-20 23:30:02 +0300

    feat: turn navbar to be sticky & refactor classes in navbar
    * create `line-clamp` utility classes & add .container utility class
    * add color reset for <a> tags, and remove inline style from navbar

--- src/components/shared/navbar/index.jsx
+++ src/components/shared/navbar/index.jsx
@@ -13,53 +13,47 @@ export default function Navbar() {
 
     return (
         <header className="navbar">
-            <Link to="/" className="navbar__logo">
-                <TempraLogo />
-            </Link>
+            <div className="navbar__wrapper container">
+                <Link to="/" className="navbar__logo">
+                    <TempraLogo />
+                </Link>
 
-            <div className="navbar__actions-container">
-                <div className="navbar__actions">
-                    {session?.isLoggedIn ? (
-                        <>
-                            <Button
-                                className={cls(
-                                    buttonVariants.faded.primary,
-                                    "d-flex align-items-center",
-                                )}
-                            >
-                                <PlusIcon className="fs-body me-1" />
-                                New time capsule
-                            </Button>
-                            <Link
-                                to="/my-capsules"
-                                style={{ color: "currentColor" }}
-                                className="fs-button"
-                            >
-                                My Capsules
-                            </Link>
-                        </>
-                    ) : (
-                        <>
-                            <Link
-                                to="/signup"
-                                className={cls(
-                                    "btn fs-button",
-                                    buttonVariants.contained.gray,
-                                )}
-                            >
-                                Sign up
-                            </Link>
-                            <Link
-                                to="/login"
-                                style={{ color: "currentColor" }}
-                                className="fs-button"
-                            >
-                                Log in
-                            </Link>
-                        </>
-                    )}
+                <div className="navbar__actions-wrapper">
+                    <div className="navbar__actions">
+                        {session?.isLoggedIn ? (
+                            <>
+                                <Button
+                                    className={cls(
+                                        buttonVariants.faded.primary,
+                                        "d-flex align-items-center",
+                                    )}
+                                >
+                                    <PlusIcon className="fs-body me-1" />
+                                    New time capsule
+                                </Button>
+                                <Link to="/my-capsules" className="fs-button">
+                                    My Capsules
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link
+                                    to="/signup"
+                                    className={cls(
+                                        "btn fs-button",
+                                        buttonVariants.contained.gray,
+                                    )}
+                                >
+                                    Sign up
+                                </Link>
+                                <Link to="/login" className="fs-button">
+                                    Log in
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                    {session?.isLoggedIn && <UserAvatarDropdown />}
                 </div>
-                {session?.isLoggedIn && <UserAvatarDropdown />}
             </div>
         </header>
     );
